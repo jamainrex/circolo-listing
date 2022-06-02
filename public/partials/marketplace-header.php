00d9c16b758ff721ac7604efd8cd2365089db7ff
Jerex Salingujay <98632127+jerexscodev@users.noreply.github.com>
2022-06-02 09:57:39 +0800

    Fixed Marketplace Header links

--- public/partials/marketplace-header.php
+++ public/partials/marketplace-header.php
@@ -1,9 +1,9 @@
 <div class="listing-preview-header">
     <ul class="listing-preview-actions">
-    <li><a class="<?php if($category == 'all' || empty($category) ) echo 'active'; ?>" href="https://circolo.club/marketplace/?category=all">All</a></li>
-    <li><a class="<?php if($category == 'experiences' ) echo 'active'; ?>" href="https://circolo.club/marketplace/?category=experiences">EXPERIENCE</a></li>
-    <li><a class="<?php if($category == 'goods' ) echo 'active'; ?>" href="https://circolo.club/marketplace/?category=goods">GOODS</a></li>
-    <li><a class="<?php if($category == 'services' ) echo 'active'; ?>" href="https://circolo.club/marketplace/?category=services">SERVICES</a></li>    
-    <li><a class="<?php if($category == 'property' ) echo 'active'; ?>" href="https://circolo.club/marketplace/?category=property">PROPERTY</a></li>
+    <li><a class="<?php if($category == 'all' || empty($category) ) echo 'active'; ?>" href="<?php echo site_url() ?>/marketplace/?category=all">All</a></li>
+	<li><a class="<?php if($category == 'goods' ) echo 'active'; ?>" href="<?php echo site_url() ?>/marketplace/?category=goods">GOODS</a></li>
+    <li><a class="<?php if($category == 'services' ) echo 'active'; ?>" href="<?php echo site_url() ?>/marketplace/?category=services">SERVICES</a></li> 
+    <li><a class="<?php if($category == 'experiences' ) echo 'active'; ?>" href="<?php echo site_url() ?>/marketplace/?category=experiences">EXPERIENCES</a></li>   
+    <li><a class="<?php if($category == 'property' ) echo 'active'; ?>" href="<?php echo site_url() ?>/marketplace/?category=property">PROPERTY</a></li>
 </ul>
 </div>
